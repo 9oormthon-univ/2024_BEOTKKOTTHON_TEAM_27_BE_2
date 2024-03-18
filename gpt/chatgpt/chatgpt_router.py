feba108d8995c8e064bace45fb8a17053e260640
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-03-19 04:10:40 +0900

    fix: chat gpt 타입 에러 해결

--- gpt/chatgpt/chatgpt_router.py
+++ gpt/chatgpt/chatgpt_router.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter, HTTPException
 from openai import OpenAI
+import requests
+import json
 
 from chatgpt.chatgpt_schema import ChatGPTRequest
 from starlette.config import Config
 
 config = Config('chatgpt/.env')
 OPENAI_KEY = config('OPENAI_KEY')
-MODEL = "gpt-3.5-turbo"
+MODEL = "gpt-4"
 
 client = OpenAI(
   api_key=OPENAI_KEY,
@@ -16,12 +18,33 @@ router = APIRouter(
 )
 
 
-@router.post("/result")
+@router.post("")
 def get_result(request: ChatGPTRequest):
-    messages = [{"role": "user", "content": request.content}]
+    url = 'https://api.openai.com/v1/chat/completions'
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + OPENAI_KEY
+    }
+
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {
+                "role": "user",
+                "content": request.content
+            }
+        ]
+    }
 
     try:
-        response = client.completions.create(model=MODEL, prompt=messages)
-        return response.choices[0].text
+        response = requests.post(url, headers=headers, json=data)
+        if response.status_code == 200:
+            response_data = json.loads(response.text)
+            content = response_data['choices'][0]['message']['content']
+            return {
+                "text": content
+            }
+        else:
+            raise HTTPException(status_code=response.status_code, detail=response.text)
     except Exception as e:
-        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
