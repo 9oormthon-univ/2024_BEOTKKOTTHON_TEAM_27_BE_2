2bc381f8ebed0ddd9bdd68906ea3a71c39048d86
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2024-03-23 23:25:17 +0900

    [Fix] 메뉴명의 글씨가 이미지를 넘어가는 문제를 해결한다 (#31)
    * refactor: media_type 수정
    
    * fix: 메뉴명 글씨 크기 조절
    
    * fix: 메뉴명 및 해시태그 스트로크 조절
    
    * refactor: 중복 코드는 클래스와 메서드로 분리한다

--- posting/editing/edit_image.py
+++ posting/editing/edit_image.py
@@ -4,6 +4,23 @@ import matplotlib.pyplot as plt
 from editing.image_size import resize_image
 from editing.add_logo import add_logo_to_image
 
+
+class custom_stroke:
+    def __init__(self, min_size: int, max_size: int):
+        self.min_size = min_size
+        self.max_size = max_size
+        self.size = max_size
+
+
+class custom_font:
+    def __init__(self, min_size: int, max_size: int, stroke: custom_stroke, font_type: str):
+        self.min_size = min_size
+        self.max_size = max_size
+        self.size = max_size
+        self.stroke = stroke
+        self.font = ImageFont.truetype(font_type, size=max_size, encoding="UTF-8")
+
+
 def put_text_on_image(image: Image, text1: str, text2: str, font_type: str, font_color: str, logo_file: str, logo_size: tuple):
     image = resize_image(image, (1000, 1000))
     image = add_logo_to_image(image, Image.open(logo_file), logo_size)
@@ -11,31 +28,34 @@ def put_text_on_image(image: Image, text1: str, text2: str, font_type: str, font
     draw = ImageDraw.Draw(image)
     image_width, image_height = image.size
 
-    font1 = ImageFont.truetype(font_type, size=200, encoding="UTF-8")
+    # font 초기화
+    font1 = custom_font(100, 190, custom_stroke(5, 18), font_type)
+    font2 = custom_font(30, 60, custom_stroke(5, 8), font_type)
 
-    font2_max_size = 100
-    font2 = ImageFont.truetype(font_type, size=font2_max_size, encoding="UTF-8")
+    font1.font = ImageFont.truetype(font_type, size=font1.max_size, encoding="UTF-8")
+    font2.font = ImageFont.truetype(font_type, size=font2.max_size, encoding="UTF-8")
 
-    text1_width, text1_height = get_text_dimensions(text1, font=font1)
-    text2_width, text2_height = get_text_dimensions(text2, font=font2)
+    # text 크기 구하기
+    text1_width, text1_height = get_text_dimensions(text1, font=font1.font)
+    text2_width, text2_height = get_text_dimensions(text2, font=font2.font)
 
-    while True:
-        if text2_width > image_width - 100 and font2_max_size > 20:
-            font2_max_size = font2_max_size - 20
-            font2 = ImageFont.truetype(font_type, font2_max_size, encoding="UTF-8")
-            text2_width, _ = get_text_dimensions(text2, font2)
-        else:
-            break
+    # font 크기 재적용
+    font1 = adjust_font_size(font1, text1, text1_width, image_width, font_type)
+    font2 = adjust_font_size(font2, text2, text2_width, image_width, font_type)
 
-    text2_width, text2_height = get_text_dimensions(text2, font=font2)
+    # text 크기 다시 구하기 (font 사이즈가 달라졌으니까)
+    text1_width, text1_height = get_text_dimensions(text1, font1.font)
+    text2_width, text2_height = get_text_dimensions(text2, font2.font)
 
+    # 폰트 위치 구하기
     x_position1 = (image_width - text1_width) // 2
-    y_position1 = image_height - text1_height
+    y_position1 = image_height - text1_height - 10
     x_position2 = (image_width - text2_width) // 2
     y_position2 = y_position1 - text2_height - 30
 
-    draw.text((x_position1, y_position1), text1, font=font1, fill=font_color, stroke_width=20, stroke_fill=(0, 0, 0))
-    draw.text((x_position2, y_position2), text2, font=font2, fill=font_color, stroke_width=8, stroke_fill=(0, 0, 0))
+    # 폰트 그리기
+    draw.text((x_position1, y_position1), text1, font=font1.font, fill=font_color, stroke_width=font1.stroke.size, stroke_fill=(0, 0, 0))
+    draw.text((x_position2, y_position2), text2, font=font2.font, fill=font_color, stroke_width=font2.stroke.size, stroke_fill=(0, 0, 0))
 
     return image
 
@@ -49,10 +69,24 @@ def get_text_dimensions(text_string, font):
     return text_width, text_height
 
 
+def adjust_font_size(custom_font: custom_font, text: str, text_width: int, image_width: int, font_type: str):
+    while True:
+        if text_width > image_width - 70 and custom_font.min_size < custom_font.size <= custom_font.max_size:
+            custom_font.size = custom_font.size - 10
+            custom_font.font = ImageFont.truetype(font_type, size=custom_font.size, encoding="UTF-8")
+            text_width, _ = get_text_dimensions(text, custom_font.font)
+
+            if custom_font.stroke.size > custom_font.stroke.min_size:
+                custom_font.stroke.size = custom_font.stroke.size - 1
+        else:
+            break
+
+    return custom_font
+
+
 if __name__ == "__main__":
-    new_image = put_text_on_image(Image.open("image/resized_tteokbokki.jpg"), "소동분식", "#매운맛쟁이 #도전해봐 #매콤한양념", "../font/SDSamliphopangcheTTFOutline.ttf", "white", "logo/logo4.png", (130, 90))
+    new_image = put_text_on_image(Image.open("image/mara.jpeg"), "가나다라마바사아자", "#마라킹 #용산구핫플 #가나다라마바사아자차", "../font/SDSamliphopangcheTTFOutline.ttf", "white", "logo/logo4.png", (130, 90))
     new_image.save("output_image.jpg")
     plt.imshow(new_image)
     plt.axis('off')
     plt.show()
-
